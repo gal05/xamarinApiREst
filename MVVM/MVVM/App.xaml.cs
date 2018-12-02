72828f5637491454a6c0c7fed1bd794267e36294
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-12-02 13:55:08 -0500

    gg

--- MVVM/MVVM/App.xaml.cs
+++ MVVM/MVVM/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVVM.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace MVVM
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new PersonaPage());
         }
 
         protected override void OnStart()
---
+++ MVVM/MVVM/Servicio/PersonaServicio.cs
@@ -0,0 +1,62 @@
+﻿using MVVM.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace MVVM.Servicio
+{
+    public class PersonaServicio
+    {
+        public ObservableCollection<PersonaModel> personas { get; set; }
+        private const string URL = "http://localhost:3000/personas";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<PersonaModel> _post;
+
+
+        public PersonaServicio()
+        {
+            if (personas == null)
+            {
+                personas = new ObservableCollection<PersonaModel>();
+            }
+        }
+
+
+        public override async ObservableCollection<PersonaModel> Consultar()
+        {
+            string content = await client.GetStringAsync(URL);
+            List<PersonaModel> data = JsonConvert.DeserializeObject<List<PersonaModel>>(content);
+            return data;
+        }
+        public void Guardar(PersonaModel modelo)
+        {
+            personas.Add(modelo);
+        }
+        public void Modificar(PersonaModel modelo)
+        {
+            for (int i = 0; i < personas.Count; i++)
+            {
+                if (personas[i].Id==modelo.Id)
+                {
+                    personas[i] = modelo;
+                }
+
+            }
+        }
+        public void eliminar(string idPersona)
+        {
+            PersonaModel modelo = personas.FirstOrDefault(p => p.Id == idPersona);
+            personas.Remove(modelo);
+        }
+    }
+}
+
+
+
+
+
+
